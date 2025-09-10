7cb3ad4c0cbf05d78da0ce3a7214a9d2b3a384b9
Tapas-gif <tapashkumarparida38@gmail.com>
2025-09-10 21:02:44 +0530

    get post

---
+++ note.md
@@ -0,0 +1,1 @@
+=>order of the routs matter alot
--- src/app.js
+++ src/app.js
@@ -2,6 +2,13 @@ const express=require("express");
 
 const app = express();
 
+app.get("/hello/2", (req,res)=>{
+    res.send("hello 2");
+});
+app.post("/hello", (req,res)=>{
+    res.send("hello");
+})
+
 app.use("/" , (req,res)=>{
     res.send("hello world");
 });
